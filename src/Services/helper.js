f2d8ef6db6dffbca61d5d154f92155e4f111595b
devagra28 <114013874+devagra28@users.noreply.github.com>
2023-11-21 15:19:11 +0530

    Update helper.js

--- src/Services/helper.js
+++ src/Services/helper.js
@@ -1,3 +1,4 @@
 // export const BACKEND_URL = "http://localhost:5000"
 //export const BACKEND_URL = "https://backend-minorprojecthandler.vercel.app"
-export const BACKEND_URL = "https://minorprojecthandlerbackend.onrender.com"
+// export const BACKEND_URL = "https://minorprojecthandlerbackend.onrender.com"
+export const BACKEND_URL = "https://mujminor-projectbackend.onrender.com"
